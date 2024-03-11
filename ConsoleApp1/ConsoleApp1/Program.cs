52520f39c4ce666312afa8bc3b9bf5d0d55c692e
Daniel Krzemiński <s28578@pjwstk.edu.pl>
2024-03-11 11:38:55 +0100

    dodana srednia

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+        //Modyfikacja 1
+        //Modyfikacja 2
+        //Modyfikacja 3
+    }
 
-//Modyfikacja 1
-//Modyfikacja 2
-//Modyfikacja 3
+    public static void WyliczSrednia(int[] liczby)
+    {
+        int suma = 0;
+        foreach (int x in liczby)
+        {
+            suma += x;
+        }
+
+        int srednia;
+        srednia = suma / liczby.Length;
+    }
+    
+}
