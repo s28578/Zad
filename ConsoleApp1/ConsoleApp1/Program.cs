36fce316096f29773b3a37f8c71bcb9214929291
Daniel Krzeminski <dkrzem37@gmail.com>
2024-03-16 14:25:22 +0100

    Zmieniona metoda WyliczSrednia

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,9 +13,9 @@ class Program
     public static void WyliczSrednia(int[] liczby)
     {
         int suma = 0;
-        foreach (int x in liczby)
+        foreach (int t in liczby)
         {
-            suma += x;
+            suma += t;
         }
 
         int srednia;
